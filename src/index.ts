3d16955a86109564da0466a2c24c97386a4512b1
Vladislav Shkvarun <neshkvarun000@gmail.com>
2023-08-14 11:08:32 +0200

    feat: add sign in and sign up layout

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import './styles/style.css';
 import './pages/routing.ts';
 import './pages/global/global.ts';
 import './pages/home/home.ts';
+import './pages/login';
+import './pages/register';
 
 function test() {
     console.log('Asynchronous sloths test for test');
