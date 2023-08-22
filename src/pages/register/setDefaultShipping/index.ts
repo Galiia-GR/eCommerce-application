8f5ff2e1d6c99ce330a0aafec98733a8d95dd8f1
Vladislav Shkvarun <neshkvarun000@gmail.com>
2023-08-22 19:50:59 +0200

    feat: fix versions

--- src/pages/register/setDefaultShipping/index.ts
+++ src/pages/register/setDefaultShipping/index.ts
@@ -6,7 +6,7 @@ export async function updateDefShippingCustomer(id: string, dataId: string): Pro
     const response = await axios.post(
         `https://api.us-central1.gcp.commercetools.com/ecommercerszxc22845345034582/customers/${id}`,
         {
-            version: 5,
+            version: 6,
             actions: [
                 {
                     action: 'setDefaultShippingAddress',
