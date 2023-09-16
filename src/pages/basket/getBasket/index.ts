624f2a5d8497955a8d4445f049e60a5ecdfaa3a1
Vladislav Shkvarun <neshkvarun000@gmail.com>
2023-09-16 06:11:09 +0200

    feat: add get func

---
+++ src/pages/basket/getBasket/index.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+export async function getBasket(basketId: string): Promise<string | undefined> {
+    if (localStorage.getItem('basket')) {
+        let response;
+
+        if (localStorage.getItem('accessToken')) {
+            try {
+                response = await axios.get(
+                    `https://api.us-central1.gcp.commercetools.com/ecommercerszxc22845345034582/me/carts/${basketId}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                );
+            } catch {
+                response = await axios.get(
+                    `https://api.us-central1.gcp.commercetools.com/ecommercerszxc22845345034582/me/carts/${basketId}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem('anonymousToken')}`,
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                );
+            }
+        } else {
+            response = await axios.get(
+                `https://api.us-central1.gcp.commercetools.com/ecommercerszxc22845345034582/me/carts/${basketId}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('anonymousToken')}`,
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+        }
+
+        localStorage.setItem('basketVersion', response.data.version);
+        console.log(await response.data);
+        return response.data;
+    }
+    return undefined;
+}
